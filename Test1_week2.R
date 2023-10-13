a7a9c91d09907b5542bee3cf665fddfa72904e17
massuyra <a.massuyr@gmail.com>
2023-10-13 15:54:48 +0100

    function

--- Test1_week2.R
+++ Test1_week2.R
@@ -1,5 +1,11 @@
 people <- c("s1", "s2", "s3", "s4", "S5", "me", "pr")
 activity <- c("learn", "learn", "learn", "learn", "learn", "learn", "teach")
 classroom <- data.frame(people, activity)
-library()
+library(tidyverse)
 view(classroom)
+show_me_activity <- function(data){
+  me_activity <- data[data$people == "me", "activity"]
+  return(me_activity)
+}
+show_me_activity(classroom)
+  
