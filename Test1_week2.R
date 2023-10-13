893f2ac64e104cda003f4496be192ee5590f4113
massuyra <a.massuyr@gmail.com>
2023-10-13 15:49:43 +0100

    added test1_week2

---
+++ Test1_week2.R
@@ -0,0 +1,5 @@
+people <- c("s1", "s2", "s3", "s4", "S5", "me", "pr")
+activity <- c("learn", "learn", "learn", "learn", "learn", "learn", "teach")
+classroom <- data.frame(people, activity)
+library()
+view(classroom)
